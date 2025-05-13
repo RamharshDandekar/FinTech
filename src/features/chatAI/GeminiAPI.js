1102633aa80594fe2437fb68bdcefac4b296ba63
Ramharsh Dandekar <ramsdandekar@gmail.com>
2025-05-13 20:01:15 +0530

    Update GeminiAPI.js

--- src/features/chatAI/GeminiAPI.js
+++ src/features/chatAI/GeminiAPI.js
@@ -4,7 +4,7 @@ const GEMINI_API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 
 async function generateContent(prompt) {
   const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro-latest" }); // Use latest Pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" }); // Use latest Pro model
 
   try {
     const result = await model.generateContent(prompt);
